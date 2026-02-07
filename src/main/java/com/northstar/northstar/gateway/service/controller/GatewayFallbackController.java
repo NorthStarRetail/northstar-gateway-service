4a090d29ae67b08e05856bde1cc5855253015e76
Saint Deemene <st.deemene@gmail.com>
2026-02-07 10:03:08 +0000

    updated

--- src/main/java/com/northstar/northstar/gateway/service/controller/GatewayFallbackController.java
+++ src/main/java/com/northstar/northstar/gateway/service/controller/GatewayFallbackController.java
@@ -4,14 +4,15 @@ import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class GatewayFallbackController {
 
     @Operation(summary = "auth service fallback", description = "Auth Service FallBack", tags = {"FallBack"})
-    @GetMapping("/auth-fallback")
+    @RequestMapping(path = "/auth-fallback", method = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE, RequestMethod.PATCH})
     ResponseEntity<String> authServiceFallback() {
         return new ResponseEntity<>(
                 "the auth service is unavailable, please try later",
@@ -19,7 +20,7 @@ public class GatewayFallbackController {
     }
 
     @Operation(summary = "book service fallback", description = "Book Service FallBack", tags = {"FallBack"})
-    @GetMapping("/book-fallback")
+    @RequestMapping(path = "/book-fallback", method = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE, RequestMethod.PATCH})
     ResponseEntity<String> bookServiceFallback() {
         return new ResponseEntity<>(
                 "the book service is unavailable, please try later",
@@ -27,7 +28,7 @@ public class GatewayFallbackController {
     }
 
     @Operation(summary = "store service fallback", description = "Store Service FallBack", tags = {"FallBack"})
-    @GetMapping("/store-fallback")
+    @RequestMapping(path = "/store-fallback", method = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE, RequestMethod.PATCH})
     ResponseEntity<String> storeServiceFallback() {
         return new ResponseEntity<>(
                 "the store service is unavailable, please try later",
@@ -35,7 +36,7 @@ public class GatewayFallbackController {
     }
 
     @Operation(summary = "order service fallback", description = "Order Service FallBack", tags = {"FallBack"})
-    @GetMapping("/order-fallback")
+    @RequestMapping(path = "/order-fallback", method = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE, RequestMethod.PATCH})
     ResponseEntity<String> orderServiceFallback() {
         return new ResponseEntity<>(
                 "the order service is unavailable, please try later",
@@ -43,7 +44,7 @@ public class GatewayFallbackController {
     }
 
     @Operation(summary = "inventory service fallback", description = "Inventory Service FallBack", tags = {"FallBack"})
-    @GetMapping("/inventory-fallback")
+    @RequestMapping(path = "/inventory-fallback", method = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE, RequestMethod.PATCH})
     ResponseEntity<String> inventoryServiceFallback() {
         return new ResponseEntity<>(
                 "the inventory service is unavailable, please try later",
@@ -51,7 +52,7 @@ public class GatewayFallbackController {
     }
 
     @Operation(summary = "notification service fallback", description = "Notification Service FallBack", tags = {"FallBack"})
-    @GetMapping("/notification-fallback")
+    @RequestMapping(path = "/notification-fallback", method = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE, RequestMethod.PATCH})
     ResponseEntity<String> notificationServiceFallback() {
         return new ResponseEntity<>(
                 "the notification service is unavailable, please try later",
